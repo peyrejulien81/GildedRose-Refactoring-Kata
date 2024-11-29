2ebd0746eb356166a754d4b3455c95d8e9a2de08
Julien PEYRE <peyrejulien81@gmail.com>
2024-11-29 17:52:49 +0100

    replace conditions by switch

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,54 +3,53 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED = "Conjured";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            /*Swith (items.name) {
-                case "Aged Brie" :
+            switch (items[i].name) {
+                case AGED_BRIE :
+                    increaseQuality(items[i]);
                     break;
-                case "Backstage passes to a TAFKAL80ETC concert" :
+                case BACKSTAGE_PASSES :
+                    increaseQuality(items[i]);
+                    if (items[i].sellIn < 11) {
+                        increaseQuality(items[i]);
+                    }
+                    if (items[i].sellIn < 6) {
+                        increaseQuality(items[i]);
+                    }
                     break;
-                case "Sulfuras, Hand of Ragnaros" : // aucun traitement, on passe au suivant
+                case SULFURAS :
+                    // aucun traitement, on passe au suivant
+                    break;
+                case CONJURED:
+                    decreaseQuality(items[i]);
+                    decreaseQuality(items[i]);
                     break;
                 default:
-            }*/
-
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(items[i]);
-                    }
-            } else {
-                increaseQuality(items[i]);
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            increaseQuality(items[i]);
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            increaseQuality(items[i]);
-                        }
-                    }
+                    decreaseQuality(items[i]);
             }
 
             decreaseSellIn(items[i]);
 
+            // le produit devient périmé
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(items[i]);
-                            }
-                    } else {
+                switch (items[i].name) {
+                    case AGED_BRIE:
+                        increaseQuality(items[i]);
+                        break;
+                    case BACKSTAGE_PASSES:
                         totalLossOfQuality(items[i]);
-                    }
-                } else {
-                    increaseQuality(items[i]);
+                        break;
+                    default:
                 }
             }
         }
